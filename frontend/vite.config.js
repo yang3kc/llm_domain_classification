dcceef4dc670a147bdd333e928b495bbdca781fa
Kaicheng Yang <yang3kc@gmail.com>
2025-04-30 18:04:26 -0400

    ✨ feat(config): set base path for production environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/llm_domain_classification/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
