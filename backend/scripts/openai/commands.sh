55b3c27e6201f2692f20bc4280490e72e401fd36
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 16:30:26 -0400

    🛠️ chore(scripts): add command to parse OpenAI responses into parquet format

--- backend/scripts/openai/commands.sh
+++ backend/scripts/openai/commands.sh
@@ -1,2 +1,4 @@
 # GPT-4o-mini, for testing purposes
 uv run query_openai.py ../../../data/source/dashboard_query_list.csv openai gpt-4o-mini ../../../data/intermediate/raw_responses/openai/gpt4omini
+
+uv run parse_openai.py openai gpt-4o-mini ../../../data/intermediate/raw_responses/openai/gpt4omini ../../../data/intermediate/parsed_responses/openai/gpt4omini.parquet
