2f8f20457a3be848954d8a2e0d755ba3bce5f289
Kaicheng Yang <yang3kc@gmail.com>
2025-05-09 21:48:22 -0400

    🆕 feat(router): add new route for paper rating page

---
+++ frontend/src/views/PaperRatingView.vue
@@ -0,0 +1,14 @@
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <div class="container mx-auto">
+      <h1>Paper Rating</h1>
+    </div>
+    <Footer />
+  </div>
+</template>
