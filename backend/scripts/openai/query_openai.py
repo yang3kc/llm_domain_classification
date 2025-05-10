e0087ab607e322d49b2fa718589de44204fccb6f
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 16:21:43 -0400

    ✨ feat(script): add progress bar for domain querying

--- backend/scripts/openai/query_openai.py
+++ backend/scripts/openai/query_openai.py
@@ -1,6 +1,7 @@
 from llmdomainrating import create_api_client, get_domain_to_query
 import sys
 import os
+from tqdm import tqdm
 
 domain_list_path = sys.argv[1]
 provider = sys.argv[2]
@@ -10,9 +11,7 @@ output_root = sys.argv[-1]
 client = create_api_client(provider)
 domains_to_query = get_domain_to_query(domain_list_path, output_root)
 
-for domain in domains_to_query:
+for domain in tqdm(domains_to_query):
     resp = client.query_model(domain, model)
-    print(f"Querying {domain} with {model}...")
     with open(os.path.join(output_root, f"{domain}.txt"), "w") as f:
         f.write(resp)
-        break
