11a20a6ab3b39d90d9d8c0f18db0bae475108825
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 16:18:53 -0400

    🔧 feat(script): enhance domain querying functionality with argument inputs

--- backend/scripts/openai/query_openai.py
+++ backend/scripts/openai/query_openai.py
@@ -1,8 +1,18 @@
-from llmdomainrating import *
+from llmdomainrating import create_api_client, get_domain_to_query
+import sys
+import os
 
-client = create_api_client("openai")
+domain_list_path = sys.argv[1]
+provider = sys.argv[2]
+model = sys.argv[3]
+output_root = sys.argv[-1]
 
-resp = client.response_api("tampabay.com", "gpt-4o-mini")
+client = create_api_client(provider)
+domains_to_query = get_domain_to_query(domain_list_path, output_root)
 
-with open("tampabay.json", "w") as f:
-    f.write(resp)
+for domain in domains_to_query:
+    resp = client.query_model(domain, model)
+    print(f"Querying {domain} with {model}...")
+    with open(os.path.join(output_root, f"{domain}.txt"), "w") as f:
+        f.write(resp)
+        break
