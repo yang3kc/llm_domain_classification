1ed61dc654f6c3d87f28d2a4321f3f297461f873
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 16:22:50 -0400

    ✨ feat(commands): add script to run GPT-4o-mini queries

---
+++ backend/scripts/openai/commands.sh
@@ -0,0 +1,2 @@
+# GPT-4o-mini, for testing purposes
+uv run query_openai.py ../../../data/source/dashboard_query_list.csv openai gpt-4o-mini ../../../data/intermediate/raw_responses/openai/gpt4omini
