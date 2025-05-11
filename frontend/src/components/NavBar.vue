8d9c00001e7eb0307d2d270e7418c95545da1fb8
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 20:44:37 -0400

    🎨 style(navbar): center align menu items in the navigation bar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="navbar bg-neutral border-b border-base-content/5">
     <div class="container mx-auto">
-      <div class="flex-none">
+      <div class="flex-1 flex justify-center">
         <ul class="menu menu-horizontal px-1">
-          <li><router-link to="/" class="btn btn-neutral text-white text-xl">Results</router-link></li>
+          <li><router-link to="/" class="btn btn-neutral text-white text-xl">Dashboard</router-link></li>
           <li><router-link to="/paper-rating" class="btn btn-neutral text-white text-xl">Paper</router-link></li>
           <li><router-link to="/about" class="btn btn-neutral text-white text-xl">About</router-link></li>
         </ul>
