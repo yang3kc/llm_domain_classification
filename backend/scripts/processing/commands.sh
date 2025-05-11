32ee5fa9a59da004d2a102c8b8abd0d81c1c6f1b
Kaicheng Yang <yang3kc@gmail.com>
2025-05-11 14:09:16 -0400

    ✨ feat(commands): add new data processing commands for together models

--- backend/scripts/processing/commands.sh
+++ backend/scripts/processing/commands.sh
@@ -16,3 +16,14 @@ uv run gen_stats.py ../../../data/intermediate/parsed_responses/anthropic/claude
 uv run gen_stats.py ../../../data/intermediate/parsed_responses/xai/grok-3-beta.parquet grok-3-beta XAI
 
 uv run gen_stats.py ../../../data/intermediate/parsed_responses/xai/grok-3-mini-beta.parquet grok-3-mini-beta XAI
+
+# Together
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/together/Llama-3.3-70B-Instruct-Turbo.parquet Llama-3.3-70B-Instruct-Turbo Meta
+
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/together/Llama-4-Scout-17B-16E-Instruct.parquet Llama-4-Scout-17B-16E-Instruct Meta
+
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/together/Llama-4-Maverick-17B-128E-Instruct-FP8.parquet Llama-4-Maverick-17B-128E-Instruct-FP8 Meta
+
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/together/DeepSeek-V3.parquet DeepSeek-V3 DeepSeek
+
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/together/DeepSeek-R1.parquet DeepSeek-R1 DeepSeek
