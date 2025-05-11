8ddb0aebd6116c48e9d8632502f302309257deba
Kaicheng Yang <yang3kc@gmail.com>
2025-05-10 20:32:31 -0400

    ✨ feat(commands): add script for generating statistics from OpenAI data

---
+++ backend/scripts/processing/commands.sh
@@ -0,0 +1,3 @@
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/openai/gpt-4.1-nano-2025-04-14.parquet gpt-4.1-nano-2025-04-14 OpenAI
+
+uv run gen_stats.py ../../../data/intermediate/parsed_responses/openai/gpt-4.1-mini-2025-04-14.parquet gpt-4.1-mini-2025-04-14 OpenAI
